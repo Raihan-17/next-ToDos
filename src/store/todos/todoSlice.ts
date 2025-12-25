014912ea101cef707d231ce882990941c570e293
Raihan-17 <aburaihanrantu009@gmail.com>
2025-12-25 09:48:23 +0600

    update slice for error and pagination

--- src/store/todos/todoSlice.ts
+++ src/store/todos/todoSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { TodoState } from "@/store/todos/todoTypes";
+import { TodoState } from "./todoTypes";
+import { fetchTodos } from "./todoThunks";
 
 const initialState: TodoState = {
   todos: [],
@@ -13,7 +14,31 @@ const initialState: TodoState = {
 const todoSlice = createSlice({
   name: "todos",
   initialState,
-  reducers: {},
+  reducers: {
+    setSkip(state, action) {
+      state.skip = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      // Fetching TODOS
+      .addCase(fetchTodos.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchTodos.fulfilled, (state, action) => {
+        state.loading = false;
+        state.todos = action.payload.todos;
+        state.total = action.payload.total;
+        state.skip = action.payload.skip;
+        state.limit = action.payload.limit;
+      })
+      .addCase(fetchTodos.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload || "Something went wrong";
+      });
+  },
 });
 
+export const { setSkip } = todoSlice.actions;
 export default todoSlice.reducer;
---
+++ src/store/todos/todoThunks.ts
@@ -0,0 +1,26 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { Todo } from "./todoTypes";
+
+interface FetchTodosResponse {
+  todos: Todo[];
+  total: number;
+  skip: number;
+  limit: number;
+}
+
+export const fetchTodos = createAsyncThunk<
+  FetchTodosResponse,
+  { skip: number; limit: number },
+  { rejectValue: string }
+>("todos/fetchTodos", async ({ skip, limit }, { rejectWithValue }) => {
+  try {
+    const response = await axios.get(
+      `https://dummyjson.com/todos?limit=${limit}&skip=${skip}`
+    );
+
+    return response.data;
+  } catch (error) {
+    return rejectWithValue("Failed to fetch todos");
+  }
+});
